379287f5a6ef2239c36dbe9936f4342ebcd36b7d
Shirin Yamani <shirinyamani@shirin.ucalgary.ca>
2024-01-23 13:18:52 -0700

    client info removed added

--- src/main.py
+++ src/main.py
@@ -5,16 +5,15 @@ import time
 import requests
 from constants import *
 
-
+#SET USER AGENT
 url = "https://www.reddit.com/api/v1/me"
 # Set your user agent in the headers
 headers = {
-    'User-Agent': 'bot/1.0 by shirin_boo'}
+    'User-Agent': 'YOUR_REDDIT_APP_NAME/1.0 by YOUY_REDDIT_USERNAME'}
 # Make a request to the Reddit API
 response = requests.get(url, headers=headers)
 
 
-  # Import the time module for sleep
 def reddit_bot(sub, trigger_phrase, comment_limit=10):
     reddit = praw.Reddit(
         client_id=os.environ['client_id'],
@@ -30,7 +29,7 @@ def reddit_bot(sub, trigger_phrase, comment_limit=10):
         if trigger_phrase in comment.body.lower():
             prompt = comment.body.strip()
             print(f"Processing comment: {comment.id}, body: {comment.body}")
-            print(f"Trigger phrase found. Prompt: {prompt}")
+            print(f"Trigger phrase found. Prompt: /n {prompt}")
 
             # Call Llama 2 API
             similar_words = call_llama(prompt= prompt, model='llama2', stream=False)
@@ -43,8 +42,7 @@ def reddit_bot(sub, trigger_phrase, comment_limit=10):
             else:
                 print("Llama 2 API did not provide suggestions for the given prompt.")
 
-# Example usage
-# Example usage
+
 if __name__ == '__main__':
     reddit_bot(sub='python', trigger_phrase='sql', comment_limit=15)
 
